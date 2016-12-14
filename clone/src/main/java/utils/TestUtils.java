c3b699ecbfee9025b0b145650382dcafad6e813f
Stefan Traeger <Stefan.Traeger@doubleSlash.de>
2016-12-14 07:56:58 +0100

    Adapt project.

--- clone/src/main/java/utils/TestUtils.java
+++ clone/src/main/java/utils/TestUtils.java
@@ -2,29 +2,26 @@ package utils;
 
 public class TestUtils {
 
-	/**
-	 * The horizontal Separator for messages.
-	 */
-	private static final String SEPARATOR_HORIZONTAL = "=========================================";
+	private static final String MESSAGE_SEPARATOR = "=========================================";
 	public static final int TESTELEMENT_AMOUNT = 100000;
 
 	public static void printMessageWithHorizontalSeperator(final String message) {
-		System.out.println(SEPARATOR_HORIZONTAL);
+		System.out.println(MESSAGE_SEPARATOR);
 		System.out.println(message);
-		System.out.println(SEPARATOR_HORIZONTAL);
+		System.out.println(MESSAGE_SEPARATOR);
 	}
 
 	public static void printStartingTestMessage(final String testname) {
-		System.out.println(SEPARATOR_HORIZONTAL);
+		System.out.println(MESSAGE_SEPARATOR);
 		System.out.println("Starting test with : " + testname);
-		System.out.println(SEPARATOR_HORIZONTAL);
+		System.out.println(MESSAGE_SEPARATOR);
 	}
 
 	public static void printStopWatchResult(final String testname, final String stopWatchResult) {
-		System.out.println(SEPARATOR_HORIZONTAL);
+		System.out.println(MESSAGE_SEPARATOR);
 		System.out.println(
 				"Time for copying " + TESTELEMENT_AMOUNT + " elements with " + testname + ": " + stopWatchResult);
-		System.out.println(SEPARATOR_HORIZONTAL);
+		System.out.println(MESSAGE_SEPARATOR);
 	}
 
 }
